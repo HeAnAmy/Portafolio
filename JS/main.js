10d1b63970e0aa2d2c0f92c20c0be67e5bf3e39c
amyhean <99053774+HeAnAmy@users.noreply.github.com>
2024-05-24 01:32:42 -0600

    Se realizan modificaciones y cambio de diseño al portafolio

---
+++ JS/main.js
@@ -0,0 +1,4 @@
+// Aquí puedes agregar cualquier funcionalidad adicional con JavaScript
+document.addEventListener('DOMContentLoaded', function () {
+    console.log('Portafolio cargado y listo!');
+});
